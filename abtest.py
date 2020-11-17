00dab8f6eef865c172b1ac5c2e2b742c064065cd
yuuyafumiya <yuuyafumiya5@gmail.com>
2020-11-17 12:01:40 +0900

    First commit

---
+++ abtest.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import chi2_contingency
+from flask import Flask, redirect ,request,render_template,jsonify
+from flask_bootstrap import Bootstrap
+import json
+import requests
+
+nc = Flask(__name__)
+
+@nc.route("/")
+def check():
+    return render_template('index.html')
+
+@nc.route('/output', methods=['POST'])
+def output():
+    #json形式でURLを受け取る
+    allA = int(request.json['allA'])
+    allB = int(request.json['allB'])
+    CVA = int(request.json['CVA'])
+    CVB = int(request.json['CVB'])
+
+    a,b,c,d=chi2_contingency(np.array([[allA,CVA],[allB,CVB]]))
+
+    return_data = {"result":b}
+    return jsonify(ResultSet=json.dumps(return_data))
+
+if __name__ == '__main__':
+    nc.run(host="127.0.0.1", port=8080)
