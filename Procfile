07de071f7ba886ec79a193c8868d52b8ec6a37b4
yuuyafumiya <yuuyafumiya5@gmail.com>
2020-11-18 14:56:14 +0900

    <バージョン名(firstとか)>

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:nc --log-file -
+web: gunicorn app:app --log-file -
--- settings.py
+++
@@ -1,5 +0,0 @@
-STATIC_URL = '/static/' #追加
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
- #画像などのメディアファイルがある場合
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
